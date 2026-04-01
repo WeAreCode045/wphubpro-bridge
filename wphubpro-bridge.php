6aacaa9282cf45302a641acdaa4a9178391068f4
WeAreCode045 <maurice@code045.nl>
2026-04-01 05:51:47 +0200

    Update version to 2.7.1 in wphubpro-bridge.php

--- wphubpro-bridge.php
+++ wphubpro-bridge.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPHubPro Bridge
  * Plugin URI: https://wphub.pro/bridge
  * Description: WPHubPro Bridge is a plugin that provides a bridge between the WPHubPro platform and WordPress. It allows you to manage your WordPress site from the WPHubPro platform.
- * Version: 2.7.0
+ * Version: 2.7.1
  * Author: WPHub PRO
  * Author URI: https://wphub.pro
  */
